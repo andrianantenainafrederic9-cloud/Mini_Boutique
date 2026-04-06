cb3d0bb08fa49e3fa307f9a8787c139a06268728
MitMatRP <andrianantenainafrederic9@gmail.com>
2026-04-06 14:16:36 +0200

    modifications usertables

--- src/components/UsersTables.jsx
+++ src/components/UsersTables.jsx
@@ -1,27 +1,37 @@
 import React from 'react'
 
 function UsersTables() {
-    const users =[
-    { id: 1, name: 'Alice', age: 25 },
-    { id: 2, name: 'Bob', age: 30 },
-    { id: 3, name: 'Charlie', age: 22 },
+    const users = [
+        { id: 1, name: 'Alice', age: 25 },
+        { id: 2, name: 'Bob', age: 30 },
+        { id: 3, name: 'Charlie', age: 22 },
     ]
-  return (
-    <div>
-        <h2>iste des utilisateurs</h2>
-        <ul>
-            {
-                users.map( user => (
-                    <li key={user.id}>
-                        {user.name}:{user.age}
 
+    return (
+        <div>
+            <h2>Liste des utilisateurs</h2>
 
-                    </li>
-                ))
-            }
-        </ul>
-    </div>
-  )
+            <table border="1" cellPadding="10">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nom</th>
+                        <th>Âge</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {users.map(user => (
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.age}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default UsersTables
